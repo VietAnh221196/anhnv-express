f1136050080d1949019a4892f4491cb69d6e9a56
nguyenvietanh96 <vietanh221196@gmail.com>
2022-06-13 19:56:23 +0700

    hgfhgf

--- server.js
+++ server.js
@@ -19,10 +19,6 @@ app.get("/", (req, res) => {
 });
 
 require("./app/routes/cities2.routes")(app);
-
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
